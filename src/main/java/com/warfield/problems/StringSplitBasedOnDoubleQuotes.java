2a387787193b57388bd3e16f32ba88af3e4cba48
Koushik Jayakumar <kojayaku@cisco.com>
2023-03-22 20:10:03 +0530

    fixed an edge case

--- src/main/java/com/warfield/problems/StringSplitBasedOnDoubleQuotes.java
+++ src/main/java/com/warfield/problems/StringSplitBasedOnDoubleQuotes.java
@@ -5,18 +5,21 @@ import java.util.*;
 public class StringSplitBasedOnDoubleQuotes
 {
     public static void main(String[] args) {
-        List<String> output = splitString("hi my name is \"koushik j\" im from coimbatore \"tamil nadu\" india");
+        List<String> output = splitString("hi my name is \"koushik j\" im from coimbatore \"tamil nadu\" india",'"');
+        List<String> output2 = splitString("im working in cisco , as a software-engineer, bangalore",',');
         System.out.println(output);
+        System.out.println(output2);
     }
 
-    private static List<String> splitString(String stringWithQuote) {
+    private static List<String> splitString(String stringWithQuote, char splitByChar) {
         char[] charArray = stringWithQuote.toCharArray();
         List<String> outputList = new ArrayList<>();
         StringBuilder substring = new StringBuilder();
         boolean doubleQuoteFlag=false;
         for (char c:charArray){
-            if (c == '"'){
+            if (c == splitByChar){
                 doubleQuoteFlag= !doubleQuoteFlag;
+                continue;
             }
             if (c == ' ' && !doubleQuoteFlag){
                 outputList.add(substring.toString());
